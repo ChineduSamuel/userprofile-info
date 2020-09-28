0510e7215ac04edfc3ab327a04bb7b893dc7836c
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2020-09-28 23:10:22 +0100

    userprofile-info

--- app.js
+++ app.js
@@ -1,74 +1,117 @@
-internalUsers = JSON.parse(localStorage.getItem("internalUsers")) 
-displayUsers()
-
-function displayUsers(){
-       
-    usersPlaceholder =  '';
-   for (i=0; i<internalUsers.length;i++)
-   {
-
-       if(internalUsers[i]!=null || internalUsers[i]!=undefined )
-       {
-       usersPlaceholder +=`<div>
-       <img src="${internalUsers[i].profilePics}" style="max-height: 100px;"><br>
-       <strong>Full Name</strong> : ${internalUsers[i].fullName}<br>
-       <strong>Email</strong> : ${internalUsers[i].email}<br>
-       <strong>Phone</strong> : ${internalUsers[i].phone}<br>
-       <button  style="background-color:red; color:#FFF" onClick="deleteUser(${i})" >Delete</button>
-       <button style="background-color:green; color:#FFF" onClick="editUser(${i})" >Edit</button>
-
-   </div> <hr>`}
-
-
-   }
-   document.getElementById("users").innerHTML = usersPlaceholder
-}
-
-function deleteUser(id)
-{
-    if(confirm(`Are you sure you want to delete ${internalUsers[id].fullName} profile? `)){
-        internalUsers.splice(id,1)
-        localStorage.setItem("internalUsers", JSON.stringify(internalUsers))
-        displayUsers()
-    }
-
-}
-
-
-function addUser(){
-    
-    let newUser =
-    { "fullName" : prompt("Enter your full name"),
-        "email": prompt("Enter your email"),
-        "phone":  prompt("Enter your phone number"),
-        "profilePics":  prompt("upload your profile pics")
-
-    }
-    internalUsers.push(newUser)
-    displayUsers()
-    localStorage.setItem("internalUsers", JSON.stringify(internalUsers))
-
-    if(internalUsers==null)
+let users = [
     {
-        internalUsers=[];
-    }
-    
-}
-function editUser(id)
+        "fullName": "Malik Murtha",
+        "email": "malik@martreach.com",
+        "phone": "+094747466",
+        "profilePics": "men.jpg"
+    },
 {
-    let userDetails = 
-    { "fullName" : prompt("Enter your full name", internalUsers[id].fullName),
-        "email": prompt("Enter your email", internalUsers[id].email),
-        "phone": prompt("Enter your phone number", internalUsers[id].phone),
-        "profilePics": prompt("upload profile pics", internalUsers[id].profilePics)
-    }
-    internalUsers[id] = userDetails
-    localStorage.setItem("internalUsers", JSON.stringify(internalUsers))
-    displayUsers()
+    "fullName": "Aminu Mohammed",
+    "email": "aminu@martreach.com",
+    "phone": "+029288282",
+    "profilePics": "men.jpg"
+},
+{
+    "fullName": "Ekeso Longe",
+    "email": "ekeso@martreach.com",
+    "phone": "+029288282",
+    "profilePics": "men.jpg"
+},
+{
+    "fullName": "Chinedu Samuel",
+    "email": "chinedu@martreach.com",
+    "phone": "+029288282",
+    "profilePics": "men.jpg"
+}
+ 
+];
+localStorage.setItem("users", JSON.stringify(users))
+JSON.parse(localStorage.getItem("users"))
 
+
+displayUsers()
+function displayUsers(){
+    userCount = '';
+    for(i = 0; i <users.length; i++){
+        userCount += `
+        <div class = "user-container">
+        <div class="input-field">
+        fullName : ${users[i].fullName}<br>
+        email : ${users[i].email}<br>
+        phone : ${users[i].phone}<br>
+        profilePics : ${users[i].profilePics}<br>
+        
+        <class="onclick-btn">
+        <button onClick="Delete(${i})" id="delete">Delete</button>  
+        <button onClick="triggerEdit(${i})" id="edit">Edit</button>  
+       </div>
+       </div>
+        </div> `
+       
+    }
+    document.getElementById("users").innerHTML = userCount
+
+}
+
+ 
+
+function Delete(i){
+    if(confirm('Are you sure you want to delete this?')){
+        users.splice(i, 1)
+        localStorage.setItem("users", JSON.stringify(users))
+         displayUsers()
+    }
+}
+
+// Update users
+function Update(){
+i =  document.getElementById("index").value 
+
+    let updatedRecord = 
+    { "fullName" : document.getElementById("fullName").value,
+    "email": document.getElementById("email").value,
+    "phone": document.getElementById("phone").value,
+    "profilePics": document.getElementById("profilePics").value
+}
+users[i] = updatedRecord
+localStorage.setItem("users", JSON.stringify(users))
+displayUsers() 
+
+}
+// Fectching
+document.getElementById('update').addEventListener('click', Update)
+
+
+
+function triggerEdit(i){
+    
+    userRecord = users[i]
+    document.getElementById("fullName").value = userRecord.fullName
+    document.getElementById("email").value= userRecord.email
+    document.getElementById("phone").value = userRecord.phone
+    document.getElementById("profilePics").value = userRecord.profilePics
+    document.getElementById("index").value = i
 }
 
 
 
+//Fetching User details
+function addUser(){
+   
+let newUser ={
+    fullName: document.getElementById('fullName').value,
+    email: document.getElementById('email').value,
+    phone: document.getElementById('phone').value,
+    profilePics: document.getElementById('profilePics').value
+
+}
+
+users.push(newUser)
+localStorage.setItem("users", JSON.stringify(users))
+displayUsers() 
+}
 
 
+
+//targeting add user button
+document.getElementById('addUser').addEventListener('click', addUser)
